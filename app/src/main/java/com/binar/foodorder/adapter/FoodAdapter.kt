02a4616f69fed400d9238102cd4091784ffab5d0
@hrahmat950 <hrahmat950@gmail.com>
2023-09-07 06:07:21 +0700

    naming item_layout

--- app/src/main/java/com/binar/foodorder/adapter/FoodAdapter.kt
+++ app/src/main/java/com/binar/foodorder/adapter/FoodAdapter.kt
@@ -11,12 +11,13 @@ import com.bumptech.glide.Glide
 /**
  * Created by Rahmat Hidayat on 27/08/2023.
  */
-class FoodAdapter(private var foods: List<Food>):RecyclerView.Adapter<FoodAdapter.ViewHolder>(){
+class FoodAdapter(private var foods: List<Food>) : RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
     fun updateData(newFoods: List<Food>) {
         val diffResult = DiffUtil.calculateDiff(FoodDiffUtil(foods, newFoods))
         foods = newFoods
         diffResult.dispatchUpdatesTo(this)
     }
+
     interface OnItemClickListener {
         fun onItemClick(food: Food)
     }
@@ -26,18 +27,19 @@ class FoodAdapter(private var foods: List<Food>):RecyclerView.Adapter<FoodAdapte
     fun setOnItemClickListener(listener: OnItemClickListener) {
         itemClickListener = listener
     }
-    class ViewHolder(binding:ItemFoodBinding):RecyclerView.ViewHolder(binding.root) {
-        private var name = binding.foodName
-        private var price = binding.foodPrice
-        private var image = binding.foodImage
-       fun bind(food: Food){
-           val formattedPrice = "Rp ${food.Price.toInt()}"
-           name.text = food.name
+
+    class ViewHolder(binding: ItemFoodBinding) : RecyclerView.ViewHolder(binding.root) {
+        private var name = binding.tvFoodname
+        private var price = binding.tvFoodprice
+        private var image = binding.tvFoodimage
+        fun bind(food: Food) {
+            val formattedPrice = "Rp ${food.Price.toInt()}"
+            name.text = food.name
             price.text = formattedPrice
-           Glide.with(itemView.context)
-               .load(food.Image)
-               .into(image)
-       }
+            Glide.with(itemView.context)
+                .load(food.Image)
+                .into(image)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
