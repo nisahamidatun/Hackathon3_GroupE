f6bc99a302592fde7ee5a1e3c4646738d453188a
@hrahmat950 <hrahmat950@gmail.com>
2023-09-12 19:35:58 +0700

    add data Food

--- app/src/main/java/com/binar/foodorder/data/FoodLocalDataSource.kt
+++ app/src/main/java/com/binar/foodorder/data/FoodLocalDataSource.kt
@@ -12,6 +12,16 @@ class FoodLocalDataSource:FoodDataSource {
             Food("itiakladomudo","https://raw.githubusercontent.com/Rahmathidayat4299/AssetGambar/master/itiakladomudo.png",80000.0,"Varian dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng."),
             Food("Ayam Pop","https://raw.githubusercontent.com/Rahmathidayat4299/AssetGambar/master/ayam%20pop.png",40000.0,"Varian dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng."),
             Food("dendengbalado","https://raw.githubusercontent.com/Rahmathidayat4299/AssetGambar/master/dendengbalado.png",50000.0,"Varian dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng."),
+            Food("chad montano","https://raw.githubusercontent.com/Rahmathidayat4299/asset-food/master/chad-montano-gE28aTnlqJA-unsplash.jpg",50000.0,"chad montano Varian dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng."),
+            Food("croissant","https://raw.githubusercontent.com/Rahmathidayat4299/asset-food/master/foodiesfeed.com_cappuccino-with-croissant.jpg",50000.0,"chad montano Varian dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng."),
+            Food("cheese burger","https://raw.githubusercontent.com/Rahmathidayat4299/asset-food/master/foodiesfeed.com_cheeseburger.jpg",50000.0,"chad montano Varian dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng."),
+            Food("spaghetti","https://raw.githubusercontent.com/Rahmathidayat4299/asset-food/master/foodiesfeed.com_cherry-tomatoes-spaghetti.jpg",50000.0,"chad montano Varian dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng."),
+            Food("crispy spicy","https://raw.githubusercontent.com/Rahmathidayat4299/asset-food/master/foodiesfeed.com_crispy-spicy-chicken-wings.jpg",50000.0,"chad montano Varian dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng."),
+            Food("dimsum","https://raw.githubusercontent.com/Rahmathidayat4299/asset-food/master/foodiesfeed.com_dim-sum-dumplings.jpg",50000.0,"chad montano Varian dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng."),
+            Food("steawberry milk","https://raw.githubusercontent.com/Rahmathidayat4299/asset-food/master/foodiesfeed.com_strawberry-milk-splash.jpg",50000.0,"chad montano Varian dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng."),
+            Food("sarawut","https://raw.githubusercontent.com/Rahmathidayat4299/asset-food/master/pexels-sarawut-horpetch-12700005.jpg",50000.0,"chad montano Varian dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng."),
+            Food("xiaolongbao","https://raw.githubusercontent.com/Rahmathidayat4299/asset-food/master/foodiesfeed.com_xiaolongbao-dumplings.jpg",50000.0,"chad montano Varian dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng."),
+            Food("sushi","https://raw.githubusercontent.com/Rahmathidayat4299/asset-food/master/foodiesfeed.com_salmon-sushi-maki.jpg",50000.0,"chad montano Varian dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng."),
         )
     }
 }
